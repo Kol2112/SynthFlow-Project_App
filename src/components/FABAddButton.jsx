cce929854b18e8c41bd9dd58d64369283884ad0d
Kamil <kolarz.kamil21@gmail.com>
2026-04-29 14:56:23 +0200

    Add some responsive view

---
+++ src/components/FABAddButton.jsx
@@ -0,0 +1,9 @@
+import { IoAddCircleOutline  } from "react-icons/io5";
+import '../styles/buttons.css'
+export default function FABADDButton(){
+    return(
+        <div id="FABAddButton">
+            <IoAddCircleOutline />
+        </div>
+    )
+}
--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -5,16 +5,17 @@ import '../styles/MainPage.css'
 import emptyDashboard from '../assets/emptyDashboard.svg'
 import CreateProject from './CreateProject.jsx'
 import LatestProject from './LatestProject.jsx'
+import FABADDButton from './FABAddButton.jsx';
 import { useState } from 'react';
 export default function MainPage(){
     const [isOpen, setIsOpen] = useState(false)
-
+//  Jeżeli document.width < 320px to aside wypierdala
 
     return(
         <>
             <Navbar/>   
-            {/* <main> */}
-                {/* <Sidebar isOpen={()=>setIsOpen(true)}/>
+            <main>
+                <Sidebar isOpen={()=>setIsOpen(true)}/>
                 <div id='mainContent'>
                     <img src={emptyDashboard} alt="" />
                     <h1>It's so empty, right now...</h1>
@@ -23,9 +24,10 @@ export default function MainPage(){
 
                 <Modal isOpen={isOpen} onClose={()=>setIsOpen(false)} title='Create project'> 
                     <CreateProject />
-                </Modal>  */}
-                <LatestProject />
-            {/* </main> */}
+                </Modal> 
+                {/* <LatestProject /> */}
+                <FABADDButton />
+            </main>
         </>
     )
 }
